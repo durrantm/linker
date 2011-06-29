cfcc0f311ed355ede397acfa1c425ed54397f563
Michael Durrant <m2@snap2web.com>
2011-06-28 20:12:45 -0400

    Added named_scope for searcher

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,8 +1,18 @@
 class Link < ActiveRecord::Base
   belongs_to :group
-  acts_as_list 
   validates_presence_of :url_address
   validates_presence_of :group_id
   validates_size_of :version_number, :maximum => 10
+  acts_as_list
+
+#  named_scope :searcher, :conditions => ["url_address like ? or alt_text_like ? or version_number like ? ", search_string, search_string, search_string]
+# Not working yet mdd 5/2/2011
+=begin
+  named_scope :searcher, lambda { |search_string|
+  { :conditions => ["url_address like ? or alt_text_like ? or version_number like ? ", search_string, search_string, search_string]}
+  }
+=end
+#  end
+
 end
 # :scope => :group
