cd970a4ee1e73a1bb43e6e4863e73f432bfb9cc4
Michael Durrant <m2@snap2web.com>
2012-06-11 16:41:11 -0400

    removed old default test fies.

--- test/unit/group_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GroupTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    record = Group.new
-    assert !record.valid?
-  end
-end
