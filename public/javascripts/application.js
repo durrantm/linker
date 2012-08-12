6185c783ca53591ee351fdf86da898daaed3503b
Michael Durrant <m2@snap2web.com>
2012-08-12 12:27:45 -0400

    js that enables Focus on the first field

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 //
-//= require jquery
+//#= require jquery
+//#= require focus_on_first_field
 
 // jQueryUI Date Picker:
 $(function (){
-        $(".datepick").datepicker;
+        $(".datepick").datepicker({ dateFormat: 'yy-mm-dd'});
 });
+$(function (){
+	$('input:first').focus;
+});
+
