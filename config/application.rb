9daf6350f9b1ecd7b99b8bbce25553851b88d1ca
Michael Durrant <m2@snap2web.com>
2012-09-01 10:04:13 -0400

    Make sure ujs gets included as well as jquery

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module Linker
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
+    config.assets.enabled = false
+
+    config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
+
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
