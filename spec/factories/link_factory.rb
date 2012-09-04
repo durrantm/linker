5252167dce1d4b7b0cffa516620e2a40c5a947cc
Michael Durrant <m2@snap2web.com>
2012-09-03 21:39:48 -0400

    Remove old comments

--- spec/factories/link_factory.rb
+++ spec/factories/link_factory.rb
@@ -3,13 +3,13 @@ FactoryGirl.define do
  # @group = Group.new(:group_name => 'g', :group_description => 'g')
 
   factory :link do
-    #group @group
-    #group 
+    group {FactoryGirl.create(:group)} #:group
     url_address "http://test.com"
     alt_text "examples of common situations amnd solutions"
   end
 
   factory :invalid_link, parent: :link do
+    group {FactoryGirl.create(:group)} #:group
     url_address nil
   end
 
