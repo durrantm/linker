74f8ae0c199df69fae92e6d1d623c2ae45877dc9
Michael Durrant <m2@snap2web.com>
2013-10-29 20:14:43 -0400

    Switch to Rails Asset Compilation.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Linker
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.assets.enabled = false
+    config.assets.enabled = true
 
     config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
 
