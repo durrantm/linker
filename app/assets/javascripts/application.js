5af799047d85151305de8ca51330c3740de7bb0c
Michael Durrant <m2@snap2web.com>
2014-06-01 16:08:54 -0400

    fix assets issue in production

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,10 @@
 // This file is automatically included by javascript_include_tag :defaults
 //
 //= require_self
-//= require_tree .
+//= require jquery-1.7.2.min
+//= require jquery-ui-1.8.22.custom.min
+//= require default_on_first_input_field
+//= require row_shading
+//= require sortable_hook
+//= require date-picker
+//= require rails
