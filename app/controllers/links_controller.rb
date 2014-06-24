947ea679a0e8ddbdfbc4d5c6fcf4c6f817b94513
Michael Durrant <m2@snap2web.com>
2014-06-23 22:42:55 -0400

    Try adding back version info

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -53,7 +53,7 @@ class LinksController < ApplicationController
         @version_information = { :version => params[:version], :version_comparison => params[:version_comparison], :include_blank_version => params[:include_blank_version] }
         @version_comparison = PrepareSearch.versions(@version_information)
         @date_comparison = PrepareSearch.dates(@from, @to)
-        @conditions = '1=1' + @groups_comparison+ @date_comparison + @text_search
+        @conditions = '1=1' + @groups_comparison+ @date_comparison + @text_search + @version_comparison
         # @conditions = '1=1' + @groups_comparison+ @date_comparison + @text_search + @version_information + @version_comparison
       else
         @conditions = PrepareSearch.basic_search(@words_1)
