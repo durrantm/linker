a370a9250d42aa5c723b759945a3c48f3e955688
Michael Durrant <m2@snap2web.com>
2014-06-27 22:14:49 -0400

    74084466 - Update table width during ajax call to change Full Details / Summary

--- app/assets/javascripts/long_or_short_details.js
+++ app/assets/javascripts/long_or_short_details.js
@@ -1,6 +1,7 @@
 $(function(){
   $("a[data-toggle-description-length]='toggle'").click(function(){
     $("span.show_hide").toggleClass("shown hidden");
+    $("table").toggleClass("wide narrow");
     $.get('/toggle_full_details');
   });
 });
