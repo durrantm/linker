1fe2824a577de90d10a81eb635deb22c35f584b6
Michael Durrant <m2@snap2web.com>
2014-06-29 10:43:29 -0400

    74093468 - Login bug goes to null page sometimes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,15 +10,17 @@ class ApplicationController < ActionController::Base
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
   protect_from_forgery # :secret => '2874469e964c91d355de8f5289b6c348'
-  
-  # See ActionController::Base for details 
+
+  # See ActionController::Base for details
   # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  # from your application log (in this case, all fields with names like "password").
   # filter_parameter_logging :password
 protected
   def authorize
     unless User.find_by_id(session[:user_id])
-      session[:original_uri] = request.url #request.request_uri
+      if request.get? && !request.xhr?
+        session[:original_uri] = request.url
+      end
       flash[:notice] = "Please Log In!"
       redirect_to :controller => 'ladmin', :action => 'login'
     end
