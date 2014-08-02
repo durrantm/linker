e70bb07826953be115143dad27b690a7646c604f
Michael Durrant <m2@snap2web.com>
2014-08-02 11:33:16 -0400

    HOTFIX

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,15 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 //
+// Place your application-specific JavaScript functions and classes here
+// This file is automatically included by javascript_include_tag :defaults
+//
+//= require jquery-1.7.2.min
+//= require jquery-ui-1.8.22.custom.min
+//= require jquery_ujs
+//= require default_on_first_input_field
+//= require row_shading
+//= require long_or_short_details
+//= require sortable_hook
+//= require date-picker
 //= require_self
-//= require_tree .
