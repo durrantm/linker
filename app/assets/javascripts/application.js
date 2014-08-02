7944d5ced735b36a4928ad19b4a7656caf38489c
Michael Durrant <m2@snap2web.com>
2014-08-02 10:41:41 -0400

    76177038 - HOTFIX - All the delete links are broken for links, groups and users. They go to the show pages and do not delete. No err msg.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,6 +3,7 @@
 //
 //= require jquery-1.7.2.min
 //= require jquery-ui-1.8.22.custom.min
+//= require jquery_ujs
 //= require default_on_first_input_field
 //= require row_shading
 //= require long_or_short_details
