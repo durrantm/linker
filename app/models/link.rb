438a8f08a5f5f9a35813cf5c70e2f87e0453b842
Michael Durrant <m2@snap2web.com>
2014-08-09 00:32:10 -0400

    wip

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,7 @@ class Link < ActiveRecord::Base
   validates_presence_of :url_address
   validates_presence_of :group_id
   validates_size_of :version_number, :maximum => 10 #, :allow_nil => true
+  #before_save :verify_this_link # :verify_url, if: :url_address_changed?
   acts_as_list
 
   def valid_get?
@@ -14,6 +15,18 @@ class Link < ActiveRecord::Base
 
   def verify_this_link
     verified_date = Time.now
+  end
+
+  def verified_date=(dt)
+    verified_date=dt
+  end
+
+  private
+
+  def verify_url
+    if valid_get?
+      verify_this_link
+    end
   end
 
 end
