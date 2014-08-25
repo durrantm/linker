684b7c6269a50c586bd95eaf0fe6fa04437c2c04
Michael Durrant <m2@snap2web.com>
2014-08-24 21:13:05 -0400

    77493900 - wip - refactor of long/short working copy, next up is group shading

--- app/assets/javascripts/long_or_short_details.js
+++ app/assets/javascripts/long_or_short_details.js
@@ -1,8 +1,19 @@
 $(function(){
+  var updateUi = function (event) {
+      event.defaultPrevented;
+      $("span.show_hide").toggleClass("shown hidden");
+      $("table").toggleClass("wide narrow");
+      if (focus == 1) {
+        $("input[type='text']:first").focus();
+      }
+      $.get('/toggle_full_details');
+  };
+
   $("a[data-toggle-description-length]='toggle'").click(function(event){
-    event.preventDefault();
-    $("span.show_hide").toggleClass("shown hidden");
-    $("table").toggleClass("wide narrow");
-    $.get('/toggle_full_details');
+    updateUi(event, focus=0);
+  });
+
+  $("a[data-toggle-description-length-refocus]='toggle'").click(function(event){
+    updateUi(event, focus=1);
   });
 });
