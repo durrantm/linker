f9f0c5f6edd51c471f32f0e47605e2de8a0485b6
Michael Durrant <m2@snap2web.com>
2014-09-01 09:41:16 -0400

    77970948 - Fix blank alt text bug which stopped links index page from working

--- app/helpers/links_helper.rb
+++ app/helpers/links_helper.rb
@@ -1,12 +1,13 @@
 module LinksHelper
-  def construct_hyperlink(link_url, link_alt_text, size='long')
+  def construct_hyperlink(link_url, link_alt_text='', size='long')
     @www_max_length = size == 'long' ? 69 : 19
     @alt_text_max_length = size == 'long' ? 85 : 35
     link_url= link_url.gsub('http://','').gsub('https://','')
     link_url_no_www= link_url.gsub('http://','').gsub('https://','').gsub('www.','')
     new_url= '<a href="http://' + link_url +'" title="' + link_url + '">'
     @address_url= link_url_no_www.length > @www_max_length ? (new_url + link_url_no_www[0..(@www_max_length+5)] + '...' + '</a>') : (new_url + link_url_no_www + '</a>')
-    @new_link_alt_text= link_alt_text.length > @alt_text_max_length  ? (link_alt_text[0..(@alt_text_max_length-1)] + '...') : link_alt_text
+    alt = link_alt_text.nil? ? '' : link_alt_text
+    @new_link_alt_text= alt.length > @alt_text_max_length  ? (alt[0..(@alt_text_max_length-1)] + '...') : alt
     return @address_url, @new_link_alt_text
   end
 
