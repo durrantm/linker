d7769a84c95dc13bb24c5f64d859a11358b6549d
Michael Durrant <m2@snap2web.com>
2014-09-27 13:21:31 -0400

    79646592 - Update default_scope to new format

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,5 +2,5 @@ class Group < ActiveRecord::Base
   has_many :links, :dependent => :destroy
   validates_presence_of :group_name
   validates_size_of :group_name, :maximum => 20
-  default_scope { order('group_name')}
+  default_scope -> { order(group_name: :asc)}
 end
