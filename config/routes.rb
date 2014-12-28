bb6ff0f38a6b543e2cc9c891194e5a59b34c740c
Michael Durrant <m2@snap2web.com>
2014-12-28 11:48:24 -0500

    85191746 - Update root path to use new syntax

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,6 @@ Linker::Application.routes.draw do
   get 'verify_link/:id', to: 'links#verify_link', as: :verify_link
   get 'unverify_link/:id', to: 'links#unverify_link', as: :unverify_link
 
-  root :to => "links#index"
+  root "links#index"
 
 end
